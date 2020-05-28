40f99bcfe4c6989de51a273b183c1b39150b4ca4
nadineb <nadineb@umich.edu>
2020-05-27 18:27:43 -0700

    [add] Logo

---
+++ client/src/components/Logo/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+function Logo() {
+
+    return (
+
+        // <!-- Nadine Bundschuh Logo -->
+        <div class="uk-position-center uk-overlay-default uk-card" id="logoBox">
+            <p class="logo">Nadine<br/>Bundschuh</p>
+        </div>
+    )
+}
+
+export default Logo;
