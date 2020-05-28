0778e5feb52312abcc1025716754479140dd94f0
nadineb <nadineb@umich.edu>
2020-05-27 18:27:00 -0700

    [move] code out of app, just return router

--- client/src/App.js
+++ client/src/App.js
@@ -1,76 +1,12 @@
 import React from 'react';
-import logo from './logo.svg';
+import Router from "../src/components/Router";
 import './App.css';
 
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route,
-  Link
-} from "react-router-dom";
 
-export default function App() {
+const App = () => {
   return (
-    <Router>
-      <div>
-        <nav>
-          <ul>
-            <li>
-              <Link to="/">Home</Link>
-            </li>
-            <li>
-              <Link to="/about">About</Link>
-            </li>
-            <li>
-              <Link to="/portfolio">Portfolio</Link>
-            </li>
-            <li>
-              <Link to="/experience">Experience</Link>
-            </li>
-            <li>
-              <Link to="/contact">Contact</Link>
-            </li>
-          </ul>
-        </nav>
-
-        {/* A <Switch> looks through its children <Route>s and
-            renders the first one that matches the current URL. */}
-        <Switch>
-          <Route path="/about">
-            <About />
-          </Route>
-          <Route path="/portfolio">
-            <Portfolio />
-          </Route>
-          <Route path="/experience">
-            <Experience />
-          </Route>
-          <Route path="/Contact">
-            <Contact />
-          </Route>
-          <Route path="/">
-            <Home />
-          </Route>
-        </Switch>
-      </div>
-    </Router>
+    <Router/>
   );
 }
 
-function Home() {
-  return <h2>Home</h2>;
-}
-
-function About() {
-  return <h2>About</h2>;
-}
-
-function Portfolio() {
-  return <h2>Portfolio</h2>;
-}
-function Experience() {
-  return <h2>Experience</h2>;
-}
-function Contact() {
-  return <h2>Contact</h2>;
-}
+export default App;
