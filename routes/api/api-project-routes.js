206afc86644048d7dd26f491ac95977a10b6c57e
nadineb <nadineb@umich.edu>
2020-05-27 21:26:34 -0700

    [move] to controller

--- routes/api/api-project-routes.js
+++ routes/api/api-project-routes.js
@@ -1,34 +1,17 @@
-const db = require("../../models");
-const Router = require("express").Router;
+const projectController = require("../../controllers/projectController");
+const router = require("express").Router();
 
-const projects = {
-    getAll: (req, res) => {
-        db.Projects.find({}).then(dbRes => {res.status(200).json(dbRes)}).catch(err => res.status(400).json(err));
-    },
-    getOne: (req, res) => {
-        db.Projects.findById(req.params.id).then(dbRes => {res.status(200).json(dbRes)}).catch(err => res.status(400).json(err));
-    },
-    create: (req, res) => {
-        db.Projects.create(req.body).then(dbRes => {res.status(200).json(dbRes)}).catch(err => res.status(400).json(err));
-    },
-    update: (req, res) => {
-        db.Projects.findByIdAndUpdate(req.params.id, req.body).then(dbRes => {res.status(200).json(dbRes)}).catch(err => res.status(400).json(err));
-    },
-    delete: (req, res) => {
-        db.Projects.findByIdAndDelete(req.params.id).then(dbRes => {res.status(200).json(dbRes)}).catch(err => res.status(400).json(err));
-    }
-}
+console.log("project")
 
-const router = Router();
 
 router.route("/")
-    .get(projects.getAll)
-    .post(projects.create);
+    .get(projectController.getAll)
+    .post(projectController.create);
 
 router.route("/:id")
-    .get(projects.getOne)
-    .put(projects.update)
-    .delete(projects.delete);
+    .get(projectController.getOne)
+    .put(projectController.update)
+    .delete(projectController.delete);
 
 
 module.exports = router;
