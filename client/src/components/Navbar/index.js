f9d4cbc95e1fb93b897f8ae04272ce1263012529
nadineb <nadineb@umich.edu>
2020-05-27 21:24:32 -0700

    [update] className and change home link

--- client/src/components/Navbar/index.js
+++ client/src/components/Navbar/index.js
@@ -3,20 +3,20 @@ import { Link } from "react-router-dom";
 
 function Navbar() {
 
-    return (
-      <div class="uk-position-relative">
-
+  return (
+    <div className="uk-position-relative">
+      <div className="uk-position-top-center">
         <nav className="uk-navbar-container uk-navbar-transparent">
           <div className="uk-navbar-center">
             <ul className="uk-navbar-nav">
-              <li>
-                <Link to="/">Home</Link>
-              </li>
               <li>
                 <Link to="/about">About</Link>
               </li>
               <li>
                 <Link to="/portfolio">Portfolio</Link>
+              </li>
+              <li>
+                <Link to="/" id="name">Nadine Bundschuh</Link>
               </li>
               <li>
                 <Link to="/experience">Experience</Link>
@@ -28,7 +28,8 @@ function Navbar() {
           </div>
         </nav>
       </div>
-    );
+    </div>
+  );
 }
 
 export default Navbar;
