a8fe85752f039c3e5b85cc60e2504aa555cd3098
nadineb <nadineb@umich.edu>
2020-05-27 18:28:28 -0700

    [add] Router

---
+++ client/src/components/Router/index.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import Home from "../../pages/Home";
+import About from "../../pages/About";
+import Portfolio from "../../pages/Portfolio";
+import Experience from "../../pages/Experience";
+import Contact from "../../pages/Contact";
+import Navbar from "../Navbar";
+import LandingContainer from "../LandingContainer";
+import Logo from "../Logo";
+
+import {
+    BrowserRouter,
+    Switch,
+    Route
+  } from "react-router-dom";
+
+const Router = () => {
+    return (
+
+        <BrowserRouter>
+            <LandingContainer>
+                <Navbar/>
+                <Switch>
+                    <Route exact path="/" component={Home}/>
+                    <Route exact path="/about" component={About}/>
+                    <Route exact path="/portfolio" component={Portfolio}/>
+                    <Route exact path="/experience" component={Experience}/>
+                    <Route exact path="/contact" component={Contact}/>
+                </Switch>
+            </LandingContainer>
+            
+        </BrowserRouter>
+    );
+}
+
+export default Router;
