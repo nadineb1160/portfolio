4e786fc5f31a9efb16dd4b236c7c3bd62a517fa8
nadineb <nadineb@umich.edu>
2020-06-23 14:39:02 -0700

    [update] portfolioContainer and classNaem

--- client/src/components/PortfolioContainer/index.js
+++ client/src/components/PortfolioContainer/index.js
@@ -3,10 +3,10 @@ import PortfolioCard from "../PortfolioCard";
 
 function PortfolioContainer({ cards }) {
     return (
-        <div class="js-wrapper uk-width-2-3 uk-align-center">
+        <div className="js-wrapper uk-width-2-3 uk-align-center">
             <div uk-overflow-auto="selContainer: .uk-height-medium; selContent: .js-wrapper">
-                <div class="uk-child-width-1-1@m uk-grid-match uk-padding uk-text-center" id="uk-overlay" uk-grid>
-                    {cards.map((card) => <PortfolioCard key={card.id} card={card} />)}
+                <div className="uk-child-width-1-1@m uk-grid-match uk-padding uk-text-center" id="uk-overlay" uk-grid="true">
+                    {cards.map((card) => <PortfolioCard key={card._id} card={card} />)}
                 </div>
             </div>
         </div>
