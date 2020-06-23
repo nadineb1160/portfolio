6e39ede56c3e42907286135bfb48271534dd4f91
nadineb <nadineb@umich.edu>
2020-06-23 14:41:23 -0700

    [move] controller

--- routes/api/api-experience-routes.js
+++ routes/api/api-experience-routes.js
@@ -1,33 +1,15 @@
-const db = require("../../models");
-const Router = require("express").Router;
+const experienceController = require("../../controllers/experienceController");
+const router = require("express").Router();
 
-const experience = {
-    getAll: (req, res) => {
-        db.Experiences.find({}).then(dbRes => {res.status(200).json(dbRes)}).catch(err => res.status(400).json(err));
-    },
-    getOne: (req, res) => {
-        db.Experiences.findById(req.params.id).then(dbRes => {res.status(200).json(dbRes)}).catch(err => res.status(400).json(err));
-    },
-    create: (req, res) => {
-        db.Experiences.create(req.body).then(dbRes => {res.status(200).json(dbRes)}).catch(err => res.status(400).json(err));
-    },
-    update: (req, res) => {
-        db.Experiences.findByIdAndUpdate(req.params.id, req.body).then(dbRes => {res.status(200).json(dbRes)}).catch(err => res.status(400).json(err));
-    },
-    delete: (req, res) => {
-        db.Experiences.findByIdAndDelete(req.params.id).then(dbRes => {res.status(200).json(dbRes)}).catch(err => res.status(400).json(err));
-    }
-}
-
-const router = Router();
 
 router.route("/")
-    .get(experience.getAll)
-    .post(experience.create);
+    .get(experienceController.getAll)
+    .post(experienceController.create);
 
 router.route("/:id")
-    .get(experience.getOne).put(experience.update)
-    .delete(experience.delete);
+    .get(experienceController.getOne)
+    .put(experienceController.update)
+    .delete(experienceController.delete);
 
 
 module.exports = router;
