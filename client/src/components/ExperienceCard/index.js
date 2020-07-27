be982ee4146ebb90a7372f73c92d270d3c60fe82
nadineb <nadineb@umich.edu>
2020-07-26 17:48:57 -0700

    [add] blue to title

--- client/src/components/ExperienceCard/index.js
+++ client/src/components/ExperienceCard/index.js
@@ -3,7 +3,7 @@ import React from "react";
 function ExperienceCard({ card }) {
     return (
         <li>
-            <h3 className="uk-accordion-title">{card.position}</h3>
+            <h3 className="uk-accordion-title blue">{card.position}</h3>
             <p>{card.date} in {card.location}</p>
             <ul className="uk-accordion-content">
                 {card.workExperience.map((experience) => <li key={card.workExperience.indexOf(experience)}>{experience}</li>)}
