bf8dcbb37760673a50cc9c618586c5409e7b3e26
nadineb <nadineb@umich.edu>
2020-07-26 17:51:18 -0700

    [add] blue on title and strong tag

--- client/src/components/PortfolioCard/index.js
+++ client/src/components/PortfolioCard/index.js
@@ -4,11 +4,11 @@ function PortfolioCard({ card }) {
     return (
         <div className="overlay-content">
             <div className="uk-card uk-card-default uk-card-small uk-card-hover uk-card-body">
-                <h3 className="uk-card-title">{card.title}</h3>
+                <h3 className="uk-card-title blue">{card.title}</h3>
                 <img src={card.image} alt="grad 2018" className="rounded" />
                 <p>{card.description}</p>
                 <hr />
-                <p><span>Technologies Used: </span>{card.technologies}</p>
+                <p><strong className="blue">Technologies Used: </strong>{card.technologies}</p>
 
                 <a href={card.githubLink}>Github Repo | </a>
                 <a href={card.demoLink}>Demo</a>
